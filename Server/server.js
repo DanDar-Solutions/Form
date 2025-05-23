96f3671bf0c180cae412bb1129f6e2cdecdf6539
Watch-people-die <tugsuuvizor@gmail.com>
2025-05-23 18:59:48 +0800

    myBackend

---
+++ Server/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import { connectDB } from "./config/db.js";
+
+
+
+dotenv.config();
+const app = express();
+
+
+
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost"],
+};
+app.use(cors(corsOptions));
+
+
+
+const port = process.env.PORT || 8800;
+app.use(express.json());
+
+
+app.listen(port, () => {
+  try {
+   connectDB();
+  } catch (error) {
+    console.log("MongoDB connection failed, but server will continue running");
+  }
+  console.log(`Server started on port: ${port}`);
+});
+
+
+
+ 
