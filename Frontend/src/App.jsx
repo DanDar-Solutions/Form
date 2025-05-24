5ea6d556fb57fb57ccaa7b0b8a3deda91a321981
ERDENE Ariunbilig <arierdene0@gmail.com>
2025-05-24 16:20:34 +0800

    Smooth scroll GSAP

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,18 +5,39 @@ import ViewResponses from './pages/ViewResponses/ViewResponses';
 import './index.css';
 import Auth from './pages/auth/auth';
 
+import { useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+
 function App() {
+
+    useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+  
+    ScrollSmoother.create({
+      wrapper: "#smooth-wrapper",
+      content: "#smooth-content",
+      smooth: 1.5,
+      effects: true
+    });
+  }, []);
+  
   return (
-    <div className="app">
-      <Navbar />
-      <main>
-        <Routes>
-          <Route path="/" element={<CreateForm />} />
-          <Route path="/create" element={<CreateForm />} />
-          <Route path="/responses/:formId" element={<ViewResponses />} />
-          <Route path="/auth" element={<Auth />} />
-        </Routes>
-      </main>
+    <div id="smooth-wrapper">
+      <div id="smooth-content">
+        <div className="app">
+          <Navbar />
+          <main>
+            <Routes>
+              <Route path="/" element={<CreateForm />} />
+              <Route path="/create" element={<CreateForm />} />
+              <Route path="/responses/:formId" element={<ViewResponses />} />
+              <Route path="/auth" element={<Auth />} />
+            </Routes>
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
